a100f3d42d30cb2fafaea287cc3062f0e1235f39
Aliz <yojan.bustamante@udea.edu.co>
2022-09-17 22:11:56 +0000

    This looks more like an actual Block of a blockchain

---
+++ coin/block.py
@@ -0,0 +1,30 @@
+import time
+
+class Block ():
+    def __init__ (self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
+        self.data = data
+
+    @staticmethod
+    def mine_block (last_block, data):
+        timestamp = time.time_ns ()
+        last_hash = last_block.hash
+        hash = f"{timestamp}{last_hash}"
+
+        return Block (timestamp, last_hash, hash, data)
+
+    @staticmethod
+    def genesis ():
+        return Block (1, "and there were nothing...", "until we created light!", [])
+
+    def __repr__ (self):
+        return (
+            "{\n"
+            f"\t\"timestamp\": \"{self.timestamp}\"\n"
+            f"\t\"last_hash\": \"{self.last_hash}\"\n"
+            f"\t\"hash\": \"{self.hash}\"\n"
+            f"\t\"data\": {self.data}\n"
+            "}"
+        )
--- coin/blockchain.py
+++ coin/blockchain.py
@@ -1,21 +1,11 @@
-class Block ():
-    def __init__ (self, data):
-        self.data = data
-
-    def __repr__ (self):
-        return f"Block: {self.data}"
+from block import Block
 
 class Blockchain ():
     def __init__ (self):
-        self.chain = []
+        self.chain = [Block.genesis ()]
 
     def add_block (self, data):
-        self.chain.append (Block (data))
+        self.chain.append (Block.mine_block (self.chain [-1], data))
 
     def __repr__ (self):
         return f"Blockchain: {self.chain}"
-
-blockchain = Blockchain ()
-blockchain.add_block ("Helouda")
-blockchain.add_block ("Hello, World!")
-print (blockchain)
