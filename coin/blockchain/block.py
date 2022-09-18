f06b8eb5b724d533f92d9b090147c8848d11edcb
Aliz <yojan.bustamante@udea.edu.co>
2022-09-18 08:04:55 +0000

    Now difficulty is adjusted automatically each 4 seconds

--- coin/blockchain/block.py
+++ coin/blockchain/block.py
@@ -1,5 +1,7 @@
 import time
+
 from crypto.crypto_hash import crypto_hash
+from config import MINE_RATE
 
 GENESIS_DATA = {
     "timestamp": 1,
@@ -24,13 +26,14 @@ class Block ():
     def mine_block (last_block, data):
         timestamp = time.time_ns ()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty (last_block, timestamp)
         nonce = 0
         hash = crypto_hash (timestamp, last_hash, data, difficulty, nonce)
 
         while hash [0:difficulty] != "0" * difficulty:
             nonce += 1
             timestamp = time.time_ns ()
+            difficulty = Block.adjust_difficulty (last_block, timestamp)
             hash = crypto_hash (timestamp, last_hash, data, difficulty, nonce)
 
         return Block (timestamp, last_hash, hash, data, difficulty, nonce)
@@ -38,6 +41,16 @@ class Block ():
     @staticmethod
     def genesis ():
         return Block (**GENESIS_DATA)
+
+    @staticmethod
+    def adjust_difficulty (last_block, new_timestamp):
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+
+        if (last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+
+        return 1
 
     def __repr__ (self):
         return (
