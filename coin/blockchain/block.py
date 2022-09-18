ce08777b037fb39ba82222b1578b424482d86e88
Aliz <yojan.bustamante@udea.edu.co>
2022-09-18 07:38:53 +0000

    Added Proof-of-Work

--- coin/blockchain/block.py
+++ coin/blockchain/block.py
@@ -5,23 +5,35 @@ GENESIS_DATA = {
     "timestamp": 1,
     "last_hash": "and there were nothing...",
     "hash": "until we created light!",
-    "data": []
+    "data": [],
+    "difficulty": 3,
+    "nonce": "genesis_nonce"
 }
 
 class Block ():
-    def __init__ (self, timestamp, last_hash, hash, data):
+    def __init__ (self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
 
+        self.difficulty = difficulty
+        self.nonce = nonce
+
     @staticmethod
     def mine_block (last_block, data):
         timestamp = time.time_ns ()
         last_hash = last_block.hash
-        hash = crypto_hash (timestamp, last_hash, data)
+        difficulty = last_block.difficulty
+        nonce = 0
+        hash = crypto_hash (timestamp, last_hash, data, difficulty, nonce)
 
-        return Block (timestamp, last_hash, hash, data)
+        while hash [0:difficulty] != "0" * difficulty:
+            nonce += 1
+            timestamp = time.time_ns ()
+            hash = crypto_hash (timestamp, last_hash, data, difficulty, nonce)
+
+        return Block (timestamp, last_hash, hash, data, difficulty, nonce)
 
     @staticmethod
     def genesis ():
@@ -34,5 +46,7 @@ class Block ():
             f"\t\"last_hash\": \"{self.last_hash}\"\n"
             f"\t\"hash\": \"{self.hash}\"\n"
             f"\t\"data\": {self.data}\n"
+            f"\t\"difficulty\": {self.difficulty}\n"
+            f"\t\"nonce\": {self.nonce}\n"
             "}"
         )
